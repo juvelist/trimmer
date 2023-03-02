ca99fbf66ad109b69e305042f3339405647c8cf1
juvelist <juvelist@list.ru>
2023-03-02 11:26:12 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import {TrimmerWrapper} from './components/TrimmerWrapper';
+
+function App() {
+  return (
+    <TrimmerWrapper/>
+  );
+}
+
+export default App;
---
+++ src/components/TrimmerWrapper/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import * as Styled from './styled'
+
+export const TrimmerWrapper = () => {
+  return (
+    <div>test</div>
+  );
+};
